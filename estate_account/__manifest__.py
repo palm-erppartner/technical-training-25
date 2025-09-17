65cdd0dda147cb2f0d13d1d24d43855b04882528
Robert <robert.palm@erppartner.ee>
2025-09-17 15:00:06 +0000

    second day

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Estate Accounting",
+    "version": "18.0.1.0.0",           
+    "application": True, # <-- key to show under “Apps”
+    "depends": ["account", "estate"],
+    "data": [
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
